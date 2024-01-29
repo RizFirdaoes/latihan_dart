34fdf41a5571011b9a93726eaf7c1bb79545e8e3
Rizky Firdaus <rizkyfirdaoes8.0@gmail.com>
2024-01-29 14:43:27 +0700

    Menambhkan file null dan ternaryOp

---
+++ bin/ternaryOperator.dart
@@ -0,0 +1,16 @@
+void main(List<String> args) {
+  // var nilai = 75;
+  // String ucapan;
+
+  // if (nilai >= 75) {
+  //   ucapan = "Selamat Anda Lulus";
+  // } else {
+  //   ucapan = "Maaf Coba Lagi";
+  // }
+
+  // print(ucapan);
+
+  //Ternary Operator, stament yang lebih sederhana dari if
+  var nilai = 75;
+  var ucapan = nilai >= 75 ? 'Selamat Anda Lulus' : 'Silahkan Coba Lagi';
+}
