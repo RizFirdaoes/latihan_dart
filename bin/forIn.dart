6cee77577f4744ed52dfaaac8d3c1c62e8ab6859
Rizky Firdaus <rizkyfirdaoes8.0@gmail.com>
2024-01-30 14:57:42 +0700

    menambhakn file forIn

---
+++ bin/forIn.dart
@@ -0,0 +1,10 @@
+/**
+ * perulangan untuk menampilkan data list dan set
+ */
+void main(List<String> args) {
+  var names = <String>['udin', 'Deny', 'Bagas'];
+
+  for (var value in names) {
+    print(value);
+  }
+}
